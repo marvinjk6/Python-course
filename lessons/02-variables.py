7f4058abddd781dd7a18faef7dd778bfc985ad82
marvinjk6 <marvin.tavares10@gmail.com>
2023-04-24 13:09:53 -0300

    02 - Variables

---
+++ lessons/02-variables.py
@@ -0,0 +1,10 @@
+# in python we can't concanete numbers, we need to use comma(,) not plus(+)
+
+name = 'Tim'
+age = 25
+gender = 'Male'
+
+
+print('His name is', name, ',he is', age, ' years old, he is a ', gender)
+print('My name is ' + name + ', and I am', age, 'years old')
+
