aa628573623a9cc87b6fb5155ad9526d474b2d89
marvinjk6 <marvin.tavares10@gmail.com>
2023-04-24 14:29:02 -0300

    04 - Numbers

---
+++ lessons/04-numbers.py
@@ -0,0 +1,44 @@
+# module % - returns the remainder
+# sum +
+# subtraction -
+# division / 
+# multiplication +
+
+n1 = 20
+n2 = 2
+
+print('Sum:', n1 + n2)
+print('Module:', n1 % n2)
+print(2 * 7)
+
+### casting a number to string
+
+n3 = str(n1)
+print('n3 is a string and we can concatenate now -> ' + n3)
+
+## absolute
+
+print('abs:', abs(-5))
+
+## Max - returns the highest number
+print('Max:', max(8, 10, 12))
+
+## Min returns lowest number
+print('Min', min(8, 10, 12))
+
+## Round - above 0.5 increases to the next number, below 0.5 decreases to the previous number
+print('Round:', round(3.5))
+print('Round:', round(3.4))
+
+## bin - converts a particular number into a binary string
+print(bin(3))
+
+## * means all
+from math import *
+
+# Square
+print(sqrt(100))
+
+
+
+
