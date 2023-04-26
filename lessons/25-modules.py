5d6767eb29026fbabb7351b9fef3d3aacbe5d236
marvinjk6 <marvin.tavares10@gmail.com>
2023-04-26 03:50:54 -0300

    25 - Modules and PIP in Python

---
+++ lessons/25-modules.py
@@ -0,0 +1,10 @@
+'''
+Modules allows to get functions, class or everything present in another file or Library,
+it allows to implement and use those same functions in your project
+
+You can use your modules importing them, but modules are also hosted online
+
+PIP - is used to installing external modules from the web to your local PC
+search on google pypi
+
+'''
