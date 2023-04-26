ef692643676604207d7e83cc3e351b13118a68af
marvinjk6 <marvin.tavares10@gmail.com>
2023-04-26 02:37:49 -0300

    21 - Classes and Objects in Python

---
+++ lessons/21-classes-and-objects.py
@@ -0,0 +1,23 @@
+'''
+Python is an object oriented programming language
+
+class - to create a class
+pass - allows us bypass any error, the application won't break
+
+'''
+
+class Person:
+    #pass
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+
+p1 = Person("Marcos", 29)
+
+
+## we can delete an object
+#del p1
+
+
+
