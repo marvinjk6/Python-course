3d394f3e297151fa79f9721719bf969b9b26410c
marvinjk6 <marvin.tavares10@gmail.com>
2023-04-26 02:57:33 -0300

    23 - Python Shell

---
+++ lessons/23-python-shell.py
@@ -0,0 +1,6 @@
+'''Python interpreter or Python Shell -  is an enviroment to code python that is automatically installed
+when python is installed 
+
+on the windows search enter: idle
+it can be used to test simple things
+'''
